7889d14d7bf1abe3905982a1cd099bf5fd08a61a
Arthyshows <arthur.mauvezin@gmail.com>
2015-02-04 16:06:48 +0100

    Create project structure

---
+++ src/main/java/fr/autopdutop/ece/java/tread_safeBST/HelloWorld.java
@@ -0,0 +1,10 @@
+package fr.autopdutop.ece.java.tread_safeBST;
+
+public class HelloWorld {
+	public static final void main(String[] args) throws InterruptedException {
+		System.out.println(" Hello ␣ World ! ");
+		while (true) {
+			Thread.sleep(1);
+		}
+	}
+}
